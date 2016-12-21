b23b32641ca748e8951cb9c39155a678493a889f
Joseph <taehun9312@gmail.com>
2016-12-21 21:51:34 +0900

    Complete korean_ver

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,12 @@ class ProductsController < ApplicationController
   # for updating, downloading csv
 
   def index
+    @recommends = ['위플래쉬','싱 스트리트','드림 쏭','비긴 어게인','플로렌스','베리 굿 걸','크리미널','보이후드','우먼 인 골드','대니쉬 걸','꾸뻬씨의 행복여행','장남감이 살아있다','
+빅 아이즈','
+카페 소사이어티','
+드레스메이커','
+내가 잠들기 전에','
+아메리칸 셰프']
     @products = Product.order(:percent_error)
     respond_to do |format|
       format.html
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   get '/en', to: 'enproducts#index'
+  get '/show' => 'products#show'
   get '/home/about' => 'products#about'
   get '/products/search' => 'products#search'
   resources :products do
